6d795792e19bfcb0e0eddfbe761cdfd5ad643dac
Adrien Gallou <adriengallou@gmail.com>
2010-10-23 20:20:03 +0200

    Changed orm from doctrine to propel.

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,6 +7,6 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfPropelPlugin');
   }
 }
