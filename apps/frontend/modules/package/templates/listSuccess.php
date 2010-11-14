6af4d61c93bbda04c2f7e93d72080010834990c8
Adrien Gallou <adriengallou@gmail.com>
2010-11-14 01:09:25 +0100

    refs #28 corrected link to package from application list.

--- apps/frontend/modules/package/templates/listSuccess.php
+++ apps/frontend/modules/package/templates/listSuccess.php
@@ -7,7 +7,7 @@ Total <?php echo strtolower($title) ?> : <?php echo $pager->getTotalRecordCount(
 <?php include_partial('package/pager', array('pager' => $pager, 'module' => $module)); ?>
 <ul>
 <?php foreach ($pager as $package): ?>
-  <li><?php echo link_to($package->getName(), $module . '/show?id=' . $package->getid()); ?></li> 
+  <li><?php echo link_to($package->getName(), 'package/show?id=' . $package->getid()); ?></li> 
 <?php endforeach; ?>
 </ul>
 <?php include_partial('package/pager', array('pager' => $pager, 'module' => $module)); ?>
