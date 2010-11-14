2a0e68ece8a12c753f4d1bdf925dfb8b76e694aa
Adrien Gallou <adriengallou@gmail.com>
2010-11-14 22:46:45 +0100

    add some menu entries

--- apps/frontend/modules/default/templates/_menu.php
+++ apps/frontend/modules/default/templates/_menu.php
@@ -1,7 +1,30 @@
 <ul>
   <li><?php echo link_to('Homepage', '@homepage'); ?></li>
-  <li><?php echo link_to('Packages', 'package/list'); ?></li>
-  <li><?php echo link_to('Applications', 'application/list'); ?></li> 
+  <li>
+    Latest
+    <ul>
+      <li>Updates (security and bugfix)</li>
+      <li>Backports (new soft versions)</li>
+      <li>Packages awaiting testing</li>
+    </ul>
+  </li>
+  <li>
+    Browse applications/packages
+    <ul>
+      <li>By group</li>
+      <li>By popularity</li>
+      <li>By name</li>
+      <li><?php echo link_to('Packages', 'package/list'); ?></li>
+      <li><?php echo link_to('Applications', 'application/list'); ?></li> 
+    </ul>
+  </li>
+  <li>
+    Requests
+    <ul>
+      <li>Backports requests</li>
+      <li>New soft request</li>
+    </ul>
+  </li>
 <?php if($sf_user->isAuthenticated()) : ?>
   <li>My Account</li>
 <?php endif; ?>
