0ae08eff4b80536960bd64e2ae4dffae0b0a1e9a
Χάρης Υφαντής <sinzoda1@otenet.gr>
2010-12-18 23:55:13 +0100

    refs #66
    * various CSS improvements (Pager / Login / etc)

--- apps/frontend/modules/user/templates/loginSuccess.php
+++ apps/frontend/modules/user/templates/loginSuccess.php
@@ -3,7 +3,7 @@
     <h2>Username</h2>
     <?php echo $form['login'] ?><br />
     <h2>Password</h2>
-    <?php echo $form['password'] ?><br />
+    <?php echo $form['password'] ?><br /><br />
   <?php echo tag('input', array('type' => 'submit')) ?>
   <?php echo '</form>' ?>
 </div>
