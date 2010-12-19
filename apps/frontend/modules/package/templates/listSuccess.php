7b6dda7c5d0b59b0f0b30611a1e7316f3411692d
Samuel Verschelde <stormi@laposte.net>
2010-12-19 01:15:00 +0100

    refs #28 remove ul in ul (fixes CSS problem)

--- apps/frontend/modules/package/templates/listSuccess.php
+++ apps/frontend/modules/package/templates/listSuccess.php
@@ -10,9 +10,8 @@
 <?php include_partial('package/pager', array('pager' => $pager, 'module' => 'package', 'madbcontext' => $madbcontext)); ?>
 
 <div>
-  Total results : <span id="count"></span>
-  <ul id="results" class="packlist">
-<ul>
+Total results : <span id="count"></span>
+<ul id="results" class="packlist">
 <?php foreach ($pager as $package): ?>
   <li><?php echo link_to($package->getName(), 'package/show?id=' . $package->getid()); ?></li> 
 <?php endforeach; ?>
