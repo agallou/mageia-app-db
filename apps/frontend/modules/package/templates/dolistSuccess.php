fb53ada797417af4443d2e37a27ba6dadd6c4dcc
Adrien Gallou <adriengallou@gmail.com>
2010-12-26 14:47:19 +0100

    delete some dead code.

--- apps/frontend/modules/package/templates/dolistSuccess.php
+++
@@ -1,8 +0,0 @@
-<?php $json = array() ?>
-<?php $results = array() ?>
-<?php foreach ($pager as $package): ?>
-  <?php $results[] = array('link' => url_for('package/show?id=' . $package->getId()), 'name' => $package->getName()); ?>
-<?php endforeach; ?>
-<?php $json['results'] = $results ?>
-<?php $json['total']   = $pager->getTotalRecordCount()?>
-<?php echo json_encode($json); ?>
