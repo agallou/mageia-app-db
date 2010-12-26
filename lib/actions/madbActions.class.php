1395e8031b037328ec05d71a097985c32a0668b1
Adrien Gallou <adriengallou@gmail.com>
2010-12-26 13:01:46 +0100

    fixes #75 added defaut distrelease and application filter values.

--- lib/actions/madbActions.class.php
+++ lib/actions/madbActions.class.php
@@ -6,6 +6,7 @@ class madbActions extends sfActions
   {
     $this->madbcontext = $this->getMadbContext();
     $this->madburl     = $this->getMadbUrl();
+    $this->redirectToDefaultParameters();
   }
 
   protected function getMadbContext()
@@ -25,6 +26,31 @@ class madbActions extends sfActions
     return new madbUrl($this->getContext());
   }
 
+  protected function redirectToDefaultParameters()
+  {
+    $getParameters   = $this->getRequest()->getParameterHolder()->getAll();
+    $addedParameters = array();
+    foreach ($this->getDefaultParameters() as $name => $value)
+    {
+      if (!in_array($name, array_keys($getParameters)))
+      {
+        $addedParameters[$name] = $value;
+      }
+    }
+    if (count($addedParameters))
+    {
+      $parameters = array_merge($getParameters, $addedParameters);
+      $url = sprintf('%s/%s?%s', $this->getModuleName(), $this->getActionName(), http_build_query($parameters));
+      $this->redirect($url);
+    }
+  }
 
+  protected function getDefaultParameters()
+  {
+    return array(
+      'application' => 1,
+      'distrelease' => DistreleasePeer::getLatest()->getId(),
+    );
+  }
 
 }
