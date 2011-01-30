f6d860faab94b1be4ad9fcf245287d025efb736d
Samuel Verschelde <stormi@laposte.net>
2011-01-30 16:39:15 +0100

    refs #27 change some words

--- apps/frontend/modules/default/templates/_searchInfos.php
+++ apps/frontend/modules/default/templates/_searchInfos.php
@@ -1,5 +1,5 @@
 <?php if ($madbcontext->hasParameter('t_search')): ?>
   You searched for : <?php echo $madbcontext->getParameter('t_search') ?>
-  <?php echo link_to('Remove search', $urldelete) ?>
+  <?php echo link_to('Clear', $urldelete) ?>
 <?php endif; ?>
 
