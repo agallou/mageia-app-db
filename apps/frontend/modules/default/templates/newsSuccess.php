423c0076289526e763649ff07a2c051eac5831d3
Adrien Gallou <adriengallou@gmail.com>
2011-01-30 19:19:15 +0100

    refs #23 added content groups and introductional text.

--- apps/frontend/modules/default/templates/newsSuccess.php
+++ apps/frontend/modules/default/templates/newsSuccess.php
@@ -1,3 +1,21 @@
-<h1>News</h1>
-<i>All latest packages and if authenticated latest followed packages
-</i>
+<div class="content_group">Welcome to mageia-app-db</div>
+
+<p>This is a work in progress lacking many features, but you can already search and browse packages.</p>
+<p>Use the search form, browse by category or use the left menu.</p>
+<p>There are persistent navigation filters, which you can change at any time from the filter banner : distribution release, show only applications or all packages, media, arch, etc.</p>
+<p>Click <a href="http://mageia-app-db.tuxette.fr/projects/mageia-app-db/wiki">here</a> for more information about this project.</p>
+
+<div class="content_group">Groups</div>
+
+
+<table width="100%">
+  <tr>
+    <td>
+      <div class="content_group">Latests updates</div>
+    </td>
+    <td>
+      <div class="content_group">Latests backports</div>
+    </td>
+  </tr>
+</table>
+
