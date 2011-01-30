f476287899891a480a643d73709c1cdc69976396
Samuel Verschelde <stormi@laposte.net>
2011-01-30 18:06:34 +0100

    refs #24 improve news views

--- apps/frontend/modules/rpm/templates/listSuccess.php
+++ apps/frontend/modules/rpm/templates/listSuccess.php
@@ -10,11 +10,42 @@
 )) ?>
 
 <div>
-<ul id="results" class="packlist">
-<?php foreach ($pager as $rpm): ?>
-  <li><?php echo $rpm->getBuildtime() ?> : <?php echo link_to($rpm->getName(), $madburl->urlFor('rpm/show', $madbcontext, array('extra_parameters' => array('id' => $rpm->getid())))); ?></li> 
-<?php endforeach; ?>
-</ul>
+<table class='packlist'>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Summary</th>
+      <th>Version</th>
+      <th>Build date</th>
+      <th>Distribution<br/>release</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($pager as $rpm): ?>
+    <tr>
+      <td><?php echo link_to(
+                  $rpm->getPackage()->getName(), 
+                  $madburl->urlFor(
+                    'package/show', 
+                    $madbcontext, 
+                    array('extra_parameters' => array('id' => $rpm->getPackage()->getId()))
+                  )
+                ); ?></td>
+      <td><?php echo $rpm->getSummary() ?></td>
+      <td><?php echo link_to(
+                  $rpm->getVersion(), 
+                  $madburl->urlFor(
+                    'rpm/show', 
+                    $madbcontext, 
+                    array('extra_parameters' => array('id' => $rpm->getid()))
+                  )
+                ); ?></td>
+      <td><?php echo $rpm->getBuildtime('Y-m-d') ?></td>
+      <td><?php echo $rpm->getDistrelease()->getName() ?></td>
+    </tr> 
+  <?php endforeach; ?>
+</tbody>
+</table>
 </div>
 
 <?php include_partial('default/pager', array(
