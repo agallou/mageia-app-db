3da9672ff7cfdcb98dab4e14a578ce19e4f70032
Adrien Gallou <adriengallou@gmail.com>
2011-01-30 15:47:23 +0100

    refs #27 added link to remove search.

--- apps/frontend/modules/default/templates/_searchInfos.php
+++ apps/frontend/modules/default/templates/_searchInfos.php
@@ -1,4 +1,5 @@
 <?php if ($madbcontext->hasParameter('t_search')): ?>
   You searched for : <?php echo $madbcontext->getParameter('t_search') ?>
+  <?php echo link_to('Remove search', $urldelete) ?>
 <?php endif; ?>
 
