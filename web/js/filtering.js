23bf0968270634d425e2d594b468ce4a35f196e4
Adrien Gallou <adriengallou@gmail.com>
2011-03-19 16:13:53 +0100

    refs #118 improve filtering UI
     arch filter doesn't need to have a search field.

--- web/js/filtering.js
+++ web/js/filtering.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
   $('#arch').selectToCheckboxes({
 	    apply: function(d){afterCheckboxChange(d)},
 	    defaults: getAllVals($('#arch option[selected=selected]')),
+      searchfield: false,
 	  });
   $('#media').selectToCheckboxes({
 	    apply: function(d){afterCheckboxChange(d)},
