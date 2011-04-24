932ec96f2fa3daf072ce70864a2c19c32688cc65
Adrien Gallou <adriengallou@gmail.com>
2011-04-24 22:29:49 +0200

    refs #168 basic user management
      created backend homepage

---
+++ apps/backend/modules/default/templates/indexSuccess.php
@@ -0,0 +1,6 @@
+<h1>Backend</h1>
+<ul>
+  <li><?php echo link_to('users', 'sf_guard_user') ?></li>
+  <li><?php echo link_to('groups', 'sf_guard_user') ?></li>
+  <li><?php echo link_to('permissions', 'sf_guard_permission') ?></li>
+</ul>
