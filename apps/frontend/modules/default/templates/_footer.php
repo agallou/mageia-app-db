2d533beec9bd38c210c58baf98545782594396f5
Adrien Gallou <adriengallou@gmail.com>
2011-04-25 08:46:15 +0200

    refs #168 basic user management
      added link to the administration in the footer.

--- apps/frontend/modules/default/templates/_footer.php
+++ apps/frontend/modules/default/templates/_footer.php
@@ -1,2 +1,3 @@
 Synchronized from <a href="http://sophie.zarb.org">http://sophie.zarb.org</a>
+ - <?php echo link_to('Administration', cross_app_url_for('backend', '@homepage')) ?>
  - Version <?php echo sfConfig::get('app_version') ?>
