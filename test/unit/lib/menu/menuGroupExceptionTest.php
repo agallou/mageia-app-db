3185b725931d3b85d5d621a1fbd0fc9278bde771
Adrien Gallou <adriengallou@gmail.com>
2011-09-18 02:04:44 +0200

    change tests from lime to atoum

--- test/unit/lib/menu/menuGroupExceptionTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-include dirname(__FILE__) . '/../../../bootstrap/unit.php';
-
-$t = new lime_test(1);
-
-$e = new menuGroupException();
-$t->ok($e instanceof sfException, 'menuGroupException extends sfException');
