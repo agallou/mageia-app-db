686576064fcac170c5b325754860cae2aeb23bce
Adrien Gallou <adriengallou@gmail.com>
2011-12-25 18:50:52 +0100

    fix #212 added 404 page

---
+++ apps/frontend/modules/default/actions/error404Action.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class error404Action extends sfActions
+{
+
+    public function execute($request)
+    {
+
+    }
+
+}
---
+++ apps/frontend/modules/default/templates/error404Success.php
@@ -0,0 +1,7 @@
+<h1>Page not found.</h1>
+
+You can :
+<ul>
+    <li>Go back to the <a href="javascript: history.go(-1);">previous page</a>,</li>
+    <li>Open an issue in the mageia-app-db <a href="http://mageia-app-db.tuxette.fr/projects/mageia-app-db/issues">bug tracker.</a></li>
+</ul>
