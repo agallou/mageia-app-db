ed4a5eddde12cdee117ef1422bbfc88d4e36cef1
Samuel Verschelde <stormi@laposte.net>
2012-01-20 23:21:37 +0100

    refs #222 order distreleases in filter

--- lib/filtering/filters/distreleaseCriteriaFilter.class.php
+++ lib/filtering/filters/distreleaseCriteriaFilter.class.php
@@ -20,11 +20,32 @@ class distreleaseCriteriaFilter extends baseCriteriaFilterChoice
   public function getValues()
   {
     $criteria = new Criteria();
+    $criteria->addDescendingOrderByColumn(DistreleasePeer::NAME);
     $distreleases = DistreleasePeer::doSelect($criteria);
     $values = array();
+    // Devel release(s) first
     foreach ($distreleases as $distrelease)
     {
-      $values[$distrelease->getId()] = $distrelease->getName();
+      if ($distrelease->getIsDevVersion())
+      {
+        $values[$distrelease->getId()] = $distrelease->getName();
+      }
+    }
+    // Then the latest stable
+    foreach ($distreleases as $distrelease)
+    {
+      if ($distrelease->getIsLatest())
+      {
+        $values[$distrelease->getId()] = $distrelease->getName();
+      }
+    }
+    // Then descending order by
+    foreach ($distreleases as $distrelease)
+    {
+      if (!array_key_exists($distrelease->getId(), $values))
+      {
+        $values[$distrelease->getId()] = $distrelease->getName();
+      }
     }
     return $values;
   }
