40bd0dc1b9af41299e129c4ea67620bd6bb901fe
Samuel Verschelde <stormi@laposte.net>
2012-01-24 22:13:30 +0100

    refs #171 forgotten file

--- lib/criteria_helper/criteriaHelperInterface.class.php
+++ lib/criteria_helper/criteriaHelperInterface.class.php
@@ -4,6 +4,4 @@ interface criteriaHelperInterface
 {
 
   public function splitPart($text, $delimiter, $count);
-  public function createTableFromCriteria(Criteria $criteria, $tablename, $temporary);
-
 }
