e97c67d1a931b829255d87a2acde9959faf732b4
Samuel Verschelde <stormi@laposte.net>
2012-05-08 13:40:28 +0200

    homepage: link to madb.org + print name from config file

--- apps/frontend/modules/default/templates/newsSuccess.php
+++ apps/frontend/modules/default/templates/newsSuccess.php
@@ -1,11 +1,11 @@
+<?php $madbConfig = new madbConfig(); ?>
 <div id="intro" class="links">
-  <div class="content_group">Welcome to mageia-app-db</div>
+  <div class="content_group">Welcome to <?php echo $madbConfig->get('name')?></div>
 
   <p>This is a work in progress lacking many features, but you can already search and browse packages. Use the search form, browse by category or use the left menu.</p>
   <p>There are persistent navigation filters, which you can change at any time from the filter banner : distribution release, show only applications or all packages, media, arch, etc.</p>
-  <p>Click <a href="http://mageia-app-db.tuxette.fr/projects/mageia-app-db/wiki">here</a> for more information about this project.</p>
+  <p>Click <a href="http://madb.org">here</a> for more information about this project.</p>
 </div>
-<?php $madbConfig = new madbConfig(); ?>
 
 <div id="groups" class="links">
   <div class="content_group">Groups</div>
