0899a89b67b455f4a6c9962e809a1a878e1427d9
Samuel Verschelde <stormi@laposte.net>
2012-05-08 10:44:16 +0200

    refs #254 put instance name in title tag too

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <?php $madbConfig = new madbConfig(); ?>
     <head>
         <?php include_http_metas() ?>
         <?php include_metas() ?>
-        <?php include_title() ?>
+        <?php echo content_tag('title', $madbConfig->get('name'))."\n"; ?>
         <link rel="shortcut icon" href="/favicon.ico" />
         <?php include_stylesheets() ?>
         <?php include_javascripts() ?>
@@ -11,7 +12,6 @@
     <body>
         <div id="container">
             <div id="header">
-                <?php $madbConfig = new madbConfig(); ?>
                 <h1><?php echo link_to($madbConfig->get('name'), '@homepage') ?></h1>
                 <div id="search">
                     <?php include_component('default', 'searching', array(
