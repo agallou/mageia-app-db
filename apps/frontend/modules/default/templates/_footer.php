98d3cbc95eb8c8cc2d7632818fbc992d050c427b
Samuel Verschelde <stormi@laposte.net>
2012-05-08 16:45:20 +0200

    refs #91 add link to AGPL + link to sources

--- apps/frontend/modules/default/templates/_footer.php
+++ apps/frontend/modules/default/templates/_footer.php
@@ -10,6 +10,8 @@
 <?php endforeach; ?>
 -
 Powered by <a href="http://madb.org">Mageia App Db</a>
+under the <a href="http://www.gnu.org/licenses/agpl-3.0.html">AGPLv3 License</a>. 
+<a href="https://gitorious.org/mageia-app-db/mageia-app-db">Get source code</a>.
 -
 Synchronized from <a href="http://sophie.zarb.org">http://sophie.zarb.org</a>
 <?php if ($sf_user->isAuthenticated() && $sf_user->hasCredential('admin')): ?>
