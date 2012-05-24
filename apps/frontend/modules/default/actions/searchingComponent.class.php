9d6344f45a03bdd3a0d7a83f3a904bb214d2bc6a
Samuel Verschelde <stormi@laposte.net>
2012-05-24 23:32:43 +0200

    refs #266 do not keep unneeded parameters in global search

--- apps/frontend/modules/default/actions/searchingComponent.class.php
+++ apps/frontend/modules/default/actions/searchingComponent.class.php
@@ -10,19 +10,25 @@ class searchingComponent extends madbComponent
     $ignored     = array('t_search', 'search', 'page');
     if ($this->module_to)
     {
-      $ignored[] = 't_group';
+      $this->showTitle = false;
+      $this->showinfos = false;
+      $keep_all_parameters = false;
+    }
+    else
+    {
+      $this->showTitle = true;
+      $this->showinfos = true;
+      $keep_all_parameters = true;
     }
     $this->url   = $this->getMadbUrl()->urlFor($internalUri, $this->getMadbContext(), array(
-      'keep_all_parameters' => true,  
+      'keep_all_parameters' => $keep_all_parameters,  
       'ignored_parameters' => $ignored,
     ));
-    $this->showTitle = !(bool)$this->module_to;
     $request           = $this->getRequest();
     $internalUri       = sprintf('%s/%s', $request['module'], $request['action']);
     $this->urldelete   = $this->getMadbUrl()->urlFor($internalUri, $this->getMadbContext(), array(
       'keep_all_parameters' => true,  
       'ignored_parameters' => array('t_search', 'search', 'page'),
     ));
-    $this->showinfos = $this->showTitle;
   }
 }
