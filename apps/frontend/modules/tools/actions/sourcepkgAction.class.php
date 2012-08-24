acd9d53c7b102385f2f9c6e8ce745281707d7d16
Samuel Verschelde <stormi@laposte.net>
2012-08-24 12:53:14 +0200

    refactoring

--- apps/frontend/modules/tools/actions/sourcepkgAction.class.php
+++ apps/frontend/modules/tools/actions/sourcepkgAction.class.php
@@ -17,99 +17,16 @@ class sourcepkgAction extends sfActions
       $this->renderText('NOTFOUND');
       return sfView::NONE;
     }
-    if ($name = $this->match($q))
+    
+    if ($source_pkg = PackagePeer::retrieveSourcePackageFromString($q, true))
     {
-      $this->renderText($name);
+      $this->renderText($source_pkg->getName());
       return sfView::NONE;
     }
 
-    // Else we try to deduce the package name
-    // just in case, only keep the package name
-    // x11-driver-video-ati-6.14.1-4.mga1.src.rpm => x11-driver-video-ati
-    $ptemp = explode('-', $q);
-    $pkg   = array();
-    foreach ($ptemp as $pi) 
-    {
-      if (is_numeric(substr($pi, 0, 1)))
-      {
-        break;
-      }
-      $pkg[] = $pi;
-    }
-    $pkg = implode('-', $pkg);
-    if ($name = $this->match($pkg))
-    {
-      $this->renderText($name);
-      return sfView::NONE;
-    }
-    
     $this->renderText('NOTFOUND');
     return sfView::NONE;
   }
   
-  private function match($q)
-  {
-    // Try to match exact SRPM name
-    $criteria = new Criteria();
-    $criteria->add(RpmPeer::NAME, $q);
-    $criteria->add(RpmPeer::IS_SOURCE, true);
-    if ($source_rpm = RpmPeer::doSelectOne($criteria))
-    {
-      return $source_rpm->getPackage()->getName();
-    }
-
-    // Try to match exact RPM name
-    $criteria = new Criteria();
-    $criteria->add(RpmPeer::NAME, $q);
-    $criteria->add(RpmPeer::IS_SOURCE, false);
-    if ($rpm = RpmPeer::doSelectOne($criteria))
-    {
-      if ($source_rpm = $rpm->getRpmRelatedBySourceRpmId())
-      {
-        return $source_rpm->getPackage()->getName();
-      }
-    }
-    
-    // Try to match a source package name
-    if (PackagePeer::retrieveByNameAndIsSource($q, true))
-    {
-      return $q;
-    }
-    
-    // Else let's see if it's a non-source package name
-    if ($package = PackagePeer::retrieveByNameAndIsSource($q, false))
-    {
-      // Get its last RPM
-      $criteria = new Criteria();
-      $criteria->add(RpmPeer::PACKAGE_ID, $package->getId());
-      $criteria->addDescendingOrderByColumn(RpmPeer::ID);
-      if ($rpm = RpmPeer::doSelectOne($criteria))
-      {
-        if ($source_rpm = $rpm->getRpmRelatedBySourceRpmId())
-        {
-          return $source_rpm->getPackage()->getName();
-        }
-      }
-    }
-    
-    // Else try to match partial SRPM name
-    $criteria = new Criteria();
-    $criteria->add(RpmPeer::NAME, $q . "%", Criteria::LIKE);
-    $criteria->add(RpmPeer::IS_SOURCE, true);
-    if ($source_rpm = RpmPeer::doSelectOne($criteria))
-    {
-      return $source_rpm->getPackage()->getName();
-    }
-
-    // Else try to match partial RPM name
-    $criteria = new Criteria();
-    $criteria->add(RpmPeer::NAME, $q . "%", Criteria::LIKE);
-    $criteria->add(RpmPeer::IS_SOURCE, false);
-    if ($source_rpm = RpmPeer::doSelectOne($criteria))
-    {
-      return $source_rpm->getPackage()->getName();
-    }
-    
-    return false;
-  }
+  
 }
--- lib/model/PackagePeer.php
+++ lib/model/PackagePeer.php
@@ -34,4 +34,147 @@ class PackagePeer extends BasePackagePeer {
     return $matches[1];
   }
 */  
+  
+  /**
+   * 
+   * Returns a source package name by various strategies:
+   * - SRPM name match
+   * - RPM name match
+   * - Source package name match
+   * - Package name match
+   * 
+   * then we try again with a stripped name (removing all version and release info)
+   * 
+   * then if it still doesn't work :
+   * - partial SRPM name match (if $partial_match = true)
+   * - partial RPM name match (if $partial_match = true)
+   * 
+   * Then it tries again after stripping versions from the string
+   * 
+   * @return Package
+   */
+  public static function retrieveSourcePackageFromString($name, $partial_match = true)
+  {
+    if ($src_package = self::exact_match($name))
+    {
+      return $src_package;
+    }
+
+    // Else we try to deduce the package name from the string
+    if ($src_package = self::exact_match(self::stripVersionFromName($name)))
+    {
+      return $src_package;
+    }
+    
+    if ($partial_match)
+    {
+      if ($src_package = self::partial_match($name))
+      {
+        return $src_package;
+      }
+
+      // Else we try to deduce the package name from the string
+      if ($src_package = self::partial_match(self::stripVersionFromName($name)))
+      {
+        return $src_package;
+      }
+    }
+    
+    return null;
+  }
+  
+  public static function stripVersionFromName($name)
+  {
+    // we try to deduce the package name
+    // x11-driver-video-ati-6.14.1-4.mga1.src.rpm => x11-driver-video-ati
+    $ptemp = explode('-', $q);
+    $pkg   = array();
+    foreach ($ptemp as $pi) 
+    {
+      if (is_numeric(substr($pi, 0, 1)))
+      {
+        break;
+      }
+      $pkg[] = $pi;
+    }
+    $pkg = implode('-', $pkg);    
+    return $pkg;
+  }
+  
+  /**
+   *
+   * Returns the closest 
+   * 
+   */
+  public static function exact_match($q)
+  {
+    // Try to match exact SRPM name
+    $criteria = new Criteria();
+    $criteria->add(RpmPeer::NAME, $q);
+    $criteria->add(RpmPeer::IS_SOURCE, true);
+    if ($source_rpm = RpmPeer::doSelectOne($criteria))
+    {
+      return $source_rpm->getPackage();
+    }
+
+    // Try to match exact RPM name
+    $criteria = new Criteria();
+    $criteria->add(RpmPeer::NAME, $q);
+    $criteria->add(RpmPeer::IS_SOURCE, false);
+    if ($rpm = RpmPeer::doSelectOne($criteria))
+    {
+      if ($source_rpm = $rpm->getRpmRelatedBySourceRpmId())
+      {
+        return $source_rpm->getPackage();
+      }
+    }
+    
+    // Try to match a source package name
+    if ($source_package = PackagePeer::retrieveByNameAndIsSource($q, true))
+    {
+      return $source_package;
+    }
+    
+    // Else let's see if it's a non-source package name
+    if ($package = PackagePeer::retrieveByNameAndIsSource($q, false))
+    {
+      // Get its last RPM
+      $criteria = new Criteria();
+      $criteria->add(RpmPeer::PACKAGE_ID, $package->getId());
+      $criteria->addDescendingOrderByColumn(RpmPeer::ID);
+      if ($rpm = RpmPeer::doSelectOne($criteria))
+      {
+        if ($source_rpm = $rpm->getRpmRelatedBySourceRpmId())
+        {
+          return $source_rpm->getPackage();
+        }
+      }
+    }
+    
+    return false;
+  }
+  
+  public static function partial_match($q)
+  {
+    // Else try to match partial SRPM name
+    $criteria = new Criteria();
+    $criteria->add(RpmPeer::NAME, $q . "%", Criteria::LIKE);
+    $criteria->add(RpmPeer::IS_SOURCE, true);
+    if ($source_rpm = RpmPeer::doSelectOne($criteria))
+    {
+      return $source_rpm->getPackage();
+    }
+
+    // Else try to match partial RPM name
+    $criteria = new Criteria();
+    $criteria->add(RpmPeer::NAME, $q . "%", Criteria::LIKE);
+    $criteria->add(RpmPeer::IS_SOURCE, false);
+    if ($source_rpm = RpmPeer::doSelectOne($criteria))
+    {
+      return $source_rpm->getPackage();
+    }
+    
+    return false;
+  }
+  
 } // PackagePeer
