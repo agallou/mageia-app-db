d580cbcd8cf826ed539fa9c2299ca83f54d94d24
Adrien Gallou <adriengallou@gmail.com>
2013-10-02 22:52:00 +0200

    some tasks don't work without the dev dependancies
    When the dependancies where installed with the option --no-dev
    the clean-propel task had an exception :
    
    exception 'InvalidArgumentException' with message 'The plugin "sfAtoumPlugin" does not exist
    
    That's because the content of sfConfig::get('sf_environment') was null
    and we test if the value is not "prod" before enabling dev plugins.
    
    Now, we check explicitly if we are on dev or test environnment before
    enabling them or if the folder sfAtoumPlugin exists and the environment
    is not production (for the case when the environment is null)

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -13,11 +13,14 @@ class ProjectConfiguration extends sfProjectConfiguration
     }
     $this->enablePlugins('sfPropelORMPlugin', 'sfGuardPlugin', 'omCrossAppUrlPlugin');
 
-    if (sfConfig::get('sf_environment') != 'prod')
+    if ((in_array(sfConfig::get('sf_environment'), array('dev', 'test')))
+       || (is_dir(sfConfig::get('sf_root_dir') . '/plugins/sfAtoumPlugin') && sfConfig::get('sf_environment') != 'prod')
+    )
     {
       require_once sfConfig::get('sf_root_dir') . '/vendor/symfony/symfony1/lib/helper/AssetHelper.php';
       $this->enablePlugins('sfAtoumPlugin', 'elXHProfPlugin');
     }
+
     sfConfig::set('sf_phing_path', sfConfig::get('sf_root_dir') .'/vendor/phing/phing');
     sfConfig::set('sf_propel_path', sfConfig::get('sf_root_dir') .'/vendor/propel/propel1');
     sfConfig::set('sf_atoum_path', dirname(__FILE__) . '/../vendor/atoum/atoum');
