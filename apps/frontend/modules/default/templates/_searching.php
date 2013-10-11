0d867c9419be8a43e7cb082f06c03a41ea679381
Samuel Verschelde <stormi@laposte.net>
2013-10-11 17:35:45 +0200

    rephrase in-page search filter

--- apps/frontend/modules/default/templates/_searching.php
+++ apps/frontend/modules/default/templates/_searching.php
@@ -1,13 +1,13 @@
 <form class="searchform" method="get" action="<?php echo $url ?>" style="display:inline">
 <?php if($showTitle): ?>
-Search in this page:
+Filter by package name:
 <?php endif; ?>
-<input type="text" name="t_search" /><input type="submit" value="search" />
+<input type="text" name="t_search" /><input type="submit" value="filter" />
 </form>
 <?php if ($showinfos): ?>
   <?php if ($madbcontext->hasParameter('t_search')): ?>
   <span class="links">
-  You searched for : <?php echo $madbcontext->getParameter('t_search') ?>
+  Current filter: <?php echo $madbcontext->getParameter('t_search') ?>
   <?php echo link_to('<i class="icon-remove"></i>', $urldelete) ?>
   </span>
   <?php endif; ?>
