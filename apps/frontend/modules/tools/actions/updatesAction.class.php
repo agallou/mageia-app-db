afe75601d342421711487126844e1529c0162d1a
Samuel Verschelde <stormi@laposte.net>
2014-02-15 22:51:54 +0100

    show validated updates waiting for a push in tools/updates

--- apps/frontend/modules/tools/actions/updatesAction.class.php
+++ apps/frontend/modules/tools/actions/updatesAction.class.php
@@ -6,7 +6,7 @@ class updatesAction extends madbActions
     // This action is very Mageia-QA-specific, should be in a mageia-specific plugin
     
     // get the list of current updates candidates from bugzilla
-    $url = "https://bugs.mageia.org/buglist.cgi?bug_status=REOPENED&bug_status=NEW&bug_status=ASSIGNED&bug_status=UNCONFIRMED&columnlist=bug_severity%2Cpriority%2Cop_sys%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cstatus_whiteboard%2Ckeywords%2Cversion%2Ccf_rpmpkg%2Ccomponent%2Cchangeddate&field0-0-0=assigned_to&field1-0-0=keywords&query_format=advanced&type0-0-0=substring&type1-0-0=notsubstring&value0-0-0=qa-bugs&value1-0-0=vali&ctype=csv";
+    $url = "https://bugs.mageia.org/buglist.cgi?bug_status=REOPENED&bug_status=NEW&bug_status=ASSIGNED&bug_status=UNCONFIRMED&columnlist=bug_severity%2Cpriority%2Cop_sys%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Cstatus_whiteboard%2Ckeywords%2Cversion%2Ccf_rpmpkg%2Ccomponent%2Cchangeddate&field0-0-0=assigned_to&query_format=advanced&type0-0-0=substring&type1-0-0=notsubstring&value0-0-0=qa-bugs&ctype=csv";
     // search URL based on RPM name
     $this->search_url = "https://bugs.mageia.org/buglist.cgi?columnlist=version%2Cassigned_to%2Cbug_status%2Cresolution%2Cshort_desc%2Ccf_rpmpkg%2Cstatus_whiteboard&email1=qa-bugs%40ml.mageia.org&emailassigned_to1=1&emailcc1=1&emailtype1=substring&field0-0-0=cf_rpmpkg&field0-0-2=short_desc&product=Mageia&query_format=advanced&type0-0-0=substring&type0-0-1=substring&type0-0-2=substring&type0-0-3=matches&value0-0-0={{SEARCH}}&value0-0-1={{SEARCH}}&value0-0-2={{SEARCH}}&value0-0-3=%22{{SEARCH}}%22&order=bug_id%20DESC&query_based_on=";
     $this->procedure_search_url = "https://wiki.mageia.org/mw-en/index.php?title=Special%3ASearch&redirs=1&search={{SEARCH}}&fulltext=Search&ns102=1&redirs=1&title=Special%3ASearch&advanced=1&fulltext=Advanced+search";
@@ -15,6 +15,7 @@ class updatesAction extends madbActions
     $rank['severity'] = 1;
     $rank['summary'] = 7;
     $rank['whiteboard'] = 8;
+    $rank['keywords'] = 9;
     $rank['version'] = 10;
     $rank['RPM'] = 11;
     $rank['component'] = 12;
@@ -110,6 +111,7 @@ class updatesAction extends madbActions
       $updates[$update[$rank['bug_id']]] = array(
           'summary'         => $update[$rank['summary']],
           'whiteboard'      => $update[$rank['whiteboard']],
+          'keywords'        => $update[$rank['keywords']],
           'versions'        => $versions,
           'RPM'             => $update[$rank['RPM']],
           'has_procedure'   => strpos($update[$rank['whiteboard']], 'has_procedure') === false ? false : true,
--- apps/frontend/modules/tools/templates/updatesSuccess.php
+++ apps/frontend/modules/tools/templates/updatesSuccess.php
@@ -7,131 +7,146 @@ See <a href="https://wiki.mageia.org/en/QA_process_for_validating_updates">QA pr
 <p>A <span class="feedback">gray background</span> means "QA team waiting for packager feedback". A star* next to the update type means that an advisory has been uploaded to SVN already.
 </p>
 <br />
-<?php foreach ($updates_by_version as $version => $updates_by_type): ?>
-<h2>Mageia <?php echo $version ?></h2>
-<table class='buglist'>
-  <thead>
-    <th>Update <br/>type</th>
-    <th>Bug number</th>
-    <th style="text-align:left;">Summary (hover for RPM name)</th>
-    <?php foreach ($archs as $arch): ?>
-    <th>Test <?php echo $arch ?></th>
-    <?php endforeach; ?>
-    <th>Proced. <br/>avail.?</th>
-    <th>Mageia <br/>Version</th>
-    <th>No action <br/>for (days)</th>
-    <th>Lists</th>
-    <th>Quick search</th>
-  </thead>
-  <tbody>
-  <?php $count = array(); ?>
-  <?php $count['total'] = 0; ?>
-  <?php foreach (array('security', 'bugfix', 'enhancement', 'backport') as $type) : ?>
-    <?php $count[$type] = 0; ?>
-    <?php if (isset($updates_by_type[$type])): ?>
-      <?php foreach ($updates_by_type[$type] as $id): ?>
-      <?php
-      $tr_class = '';
-      switch ($updates[$id]['severity'])
-      {
-        case 'enhancement':
-        case 'backport':
-          $tr_class = 'enhancement';
-          break;
-        case 'low':
-          $tr_class = 'low';
-          break;
-        case 'major':
-          $tr_class = ($type == 'security') ? 'major' : '';
-          break;
-        case 'critical':
-          $tr_class = ($type == 'security') ? 'critical' : 'major';
-          break;
-        default:
-          break;
-      }
-      if ($updates[$id]['feedback'])
-      {
-        $tr_class .= " feedback";
-      }
-      ?>
-      <tr class="<?php echo $tr_class ?>">
-        <?php $count[$type]++; ?> 
-        <?php $count['total']++; ?> 
-        <td><?php echo $type . ($updates[$id]['has_advisory'] ? '*' : '') ?></td>
-        <td><?php echo link_to($id, 'https://bugs.mageia.org/show_bug.cgi?id=' . $id) ?></td>
-        <td style="text-align:left;" title="<?php echo $updates[$id]['RPM']?>"><?php 
-        echo link_to(
-                substr($updates[$id]['summary'], 0, 100) . (strlen($updates[$id]['summary'])>100 ? "[...]" : ""), 
-                'https://bugs.mageia.org/show_bug.cgi?id=' . $id) 
-        ?></td>
-        <?php foreach ($archs as $arch): ?>
-        <td><?php 
-        if (isset($updates[$id]['testing_status'][$version][$arch]) && $updates[$id]['testing_status'][$version][$arch])
+<?php foreach (array('pending', 'validated') as $status): ?>
+  <?php if ($status == 'validated'): ?>
+  <br/><br/><br/>Below is a list of validated updates waiting to be pushed to the updates media. Those without a star* need an advisory to be uploaded, first.<br/><br/>
+  <?php endif ?>
+  <?php foreach ($updates_by_version as $version => $updates_by_type): ?>
+  <h2>Mageia <?php echo $version ?></h2>
+  <table class='buglist'>
+    <thead>
+      <th>Update <br/>type</th>
+      <th>Bug number</th>
+      <th style="text-align:left;">Summary (hover for RPM name)</th>
+      <?php foreach ($archs as $arch): ?>
+      <th>Test <?php echo $arch ?></th>
+      <?php endforeach; ?>
+      <th>Proced. <br/>avail.?</th>
+      <th>Mageia <br/>Version</th>
+      <th>No action <br/>for (days)</th>
+      <th>Lists</th>
+      <th>Quick search</th>
+    </thead>
+    <tbody>
+    <?php $count = array(); ?>
+    <?php $count['total'] = 0; ?>
+    <?php foreach (array('security', 'bugfix', 'enhancement', 'backport') as $type) : ?>
+      <?php $count[$type] = 0; ?>
+      <?php if (isset($updates_by_type[$type])): ?>
+        <?php foreach ($updates_by_type[$type] as $id): ?>
+        <?php
+        if (false !== strpos($updates[$id]['keywords'], 'validated_update') && $status == 'pending')
         {
-          echo "OK";
-          if ($updates[$id]['testing_status'][$version][$arch] == 2)
-          {
-            echo "?";
-          }
+          continue;
         }
-        else
+        if (false === strpos($updates[$id]['keywords'], 'validated_update') && $status == 'validated')
         {
-          echo "&nbsp;";
+          continue;
         }
-        ?></td>
-        <?php endforeach; ?>
-        <td><?php echo $updates[$id]['has_procedure'] ? "yes" : "&nbsp;" ?></td>
-        <td><?php 
-        foreach ($updates[$id]['versions'] as $the_version)
+        
+        
+        $tr_class = '';
+        switch ($updates[$id]['severity'])
         {
-          $testing_complete = true;
-          foreach ($archs as $arch)
+          case 'enhancement':
+          case 'backport':
+            $tr_class = 'enhancement';
+            break;
+          case 'low':
+            $tr_class = 'low';
+            break;
+          case 'major':
+            $tr_class = ($type == 'security') ? 'major' : '';
+            break;
+          case 'critical':
+            $tr_class = ($type == 'security') ? 'critical' : 'major';
+            break;
+          default:
+            break;
+        }
+        if ($updates[$id]['feedback'])
+        {
+          $tr_class .= " feedback";
+        }
+        ?>
+        <tr class="<?php echo $tr_class ?>">
+          <?php $count[$type]++; ?> 
+          <?php $count['total']++; ?> 
+          <td><?php echo $type . ($updates[$id]['has_advisory'] ? '*' : '') ?></td>
+          <td><?php echo link_to($id, 'https://bugs.mageia.org/show_bug.cgi?id=' . $id) ?></td>
+          <td style="text-align:left;" title="<?php echo $updates[$id]['RPM']?>"><?php 
+          echo link_to(
+                  substr($updates[$id]['summary'], 0, 100) . (strlen($updates[$id]['summary'])>100 ? "[...]" : ""), 
+                  'https://bugs.mageia.org/show_bug.cgi?id=' . $id) 
+          ?></td>
+          <?php foreach ($archs as $arch): ?>
+          <td><?php 
+          if (isset($updates[$id]['testing_status'][$version][$arch]) && $updates[$id]['testing_status'][$version][$arch])
           {
-            if (!isset($updates[$id]['testing_status'][$the_version][$arch]) or $updates[$id]['testing_status'][$the_version][$arch]!=1)
+            echo "OK";
+            if ($updates[$id]['testing_status'][$version][$arch] == 2)
             {
-              $testing_complete = false;
+              echo "?";
             }
-          }
-          echo "$the_version" . ($testing_complete ? '✓' : '') . " ";
-        }
-        ?></td> 
-        <td><?php 
-        $date = new DateTime(substr($updates[$id]['changed'], 0, 10));
-        echo $date->diff($now)->format("%a");
-        ?></td>
-        <td><?php echo link_to("RPMs", $madburl->urlFor("tools/listRpmsForQaBug", $madbcontext, array('extra_parameters' => array('bugnum' => $id, 'application'=>0)))); ?></td>
-        <td style="text-align:left;"><?php 
-        if ($source_package = $updates[$id]['source_package'])
-        {
-          if (is_object($source_package))
-          {
-            $name = $source_package->getName();
-            $name2 = $name;
           }
           else
           {
-            $name = $source_package;
-            $name2 = ".$name";
+            echo "&nbsp;";
           }
-          echo link_to("Bugzilla", str_replace('{{SEARCH}}', $name, $search_url));
-          echo ", ";
-          echo link_to("Wiki", str_replace('{{SEARCH}}', $name, $procedure_search_url));
-          echo " ($name2)";
-        }
-        ?></td>
-      </tr>  
-      <?php endforeach; ?>
-    <?php endif; ?>
-  <?php endforeach; ?>
+          ?></td>
+          <?php endforeach; ?>
+          <td><?php echo $updates[$id]['has_procedure'] ? "yes" : "&nbsp;" ?></td>
+          <td><?php 
+          foreach ($updates[$id]['versions'] as $the_version)
+          {
+            $testing_complete = true;
+            foreach ($archs as $arch)
+            {
+              if (!isset($updates[$id]['testing_status'][$the_version][$arch]) or $updates[$id]['testing_status'][$the_version][$arch]!=1)
+              {
+                $testing_complete = false;
+              }
+            }
+            echo "$the_version" . ($testing_complete ? '✓' : '') . " ";
+          }
+          ?></td> 
+          <td><?php 
+          $date = new DateTime(substr($updates[$id]['changed'], 0, 10));
+          echo $date->diff($now)->format("%a");
+          ?></td>
+          <td><?php echo link_to("RPMs", $madburl->urlFor("tools/listRpmsForQaBug", $madbcontext, array('extra_parameters' => array('bugnum' => $id, 'application'=>0)))); ?></td>
+          <td style="text-align:left;"><?php 
+          if ($source_package = $updates[$id]['source_package'])
+          {
+            if (is_object($source_package))
+            {
+              $name = $source_package->getName();
+              $name2 = $name;
+            }
+            else
+            {
+              $name = $source_package;
+              $name2 = ".$name";
+            }
+            echo link_to("Bugzilla", str_replace('{{SEARCH}}', $name, $search_url));
+            echo ", ";
+            echo link_to("Wiki", str_replace('{{SEARCH}}', $name, $procedure_search_url));
+            echo " ($name2)";
+          }
+          ?></td>
+        </tr>  
+        <?php endforeach; ?>
+      <?php endif; ?>
+    <?php endforeach; ?>
   </tbody>
-</table>
+  </table>
 Number of update candidates: <?php echo $count['total'] - $count['backport']; ?> 
 (security: <?php echo $count['security'] ?>,
 bugfix: <?php echo $count['bugfix'] ?>,
 enhancement: <?php echo $count['enhancement'] ?>)<br/>
 Number of backports : <?php echo $count['backport'] ?><br/>
-<br/>
+  <br/>
+  <?php endforeach; ?>
 <?php endforeach; ?>
 
 <?php use_helper('JavascriptBase') ?>
