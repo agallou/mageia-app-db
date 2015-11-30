dfe70870ea730c1564f85f88f5e901ab6e978c08
Adrien Gallou <adriengallou@gmail.com>
2015-11-30 21:47:35 +0100

    fix dbInfosFactory when host is not a string
    if we use 127.0.0.1 instead of localhost, the host and
    database are not found, and cli scripts doesn't work.
     Changing the regexp fixes that.

--- lib/dbInfos/dbInfosFactory.class.php
+++ lib/dbInfos/dbInfosFactory.class.php
@@ -10,7 +10,7 @@ class dbInfosFactory
     $infos->setPassword($connection['password']);
     $dsn = $connection['dsn'];
     $matches = array();
-    preg_match('/:host=(\w*);dbname=(.*)/', $dsn, $matches);
+    preg_match('/:host=(.*);dbname=(.*)/', $dsn, $matches);
     $infos->setHost($matches[1]);
     $infos->setName($matches[2]);
     return $infos;
